a9c3df5d9b45b37917756cd12109951bd2fe0bff
Abdullah <abdullahtarar979@gmail.com>
2021-11-26 10:57:51 +0500

    first commit

---
+++ Question 61/Question 61/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Question_61
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 20, c = 15, d = 8, e = 40;
+
+            //(1)
+            Console.WriteLine((a + b / (c - 5)) / ((d + 7) / (e - 37) % 3));
+
+            //(2)
+            Console.WriteLine(a + b / c - 5 / d + 7 / e - 37 % 3);
+
+            //(3)
+            Console.WriteLine(a * (b * b) - (c * b) + d);
+        }
+    }
+}
